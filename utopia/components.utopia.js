449af38cc763bb30fcf8de9fda0477106966ec00
Bertalan Körmendy <korber314@gmail.com>
2024-04-02 17:56:35 +0200

    ImageCropControl

--- utopia/components.utopia.js
+++ utopia/components.utopia.js
@@ -2,6 +2,14 @@ import * as Utopia from 'utopia-api'
 import { Image } from '@shopify/hydrogen'
 import { RecommendedProductsGrid } from '../app/components/Layout'
 
+const ImageCropControl = Utopia.expressionPopupListControl([
+  Utopia.expression('top', '"top"'),
+  Utopia.expression('center', '"center"'),
+  Utopia.expression('bottom', '"bottom"'),
+  Utopia.expression('left', '"left"'),
+  Utopia.expression('right', '"right"'),
+])
+
 const ImageTypeControl = Utopia.objectControl({
   altText: Utopia.stringControl(),
   id: Utopia.stringControl(),
@@ -17,7 +25,7 @@ const Components = {
       component: Image,
       properties: {
         aspectRatio: Utopia.stringControl(),
-        crop: Utopia.stringControl(),
+        crop: ImageCropControl,
         data: ImageTypeControl,
         // loader is omitted, we don't have a control description for functions yet
         srcSetOptions: Utopia.objectControl({
