88e19a290eff7467a1ab2b84cb103174fa19becb
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-04-02 16:18:05 +0200

    Extract ImageTypeControl

--- utopia/components.utopia.js
+++ utopia/components.utopia.js
@@ -1,6 +1,14 @@
 import * as Utopia from 'utopia-api'
 import { Image } from '@shopify/hydrogen'
 
+const ImageTypeControl = Utopia.objectControl({
+  altText: Utopia.stringControl(),
+  id: Utopia.stringControl(),
+  url: Utopia.stringControl(),
+  height: Utopia.numberControl(),
+  width: Utopia.numberControl(),
+})
+
 const Components = {
   '@shopify/hydrogen': {
     // https://shopify.dev/docs/api/hydrogen-react/2024-01/components/image
@@ -9,20 +17,14 @@ const Components = {
       properties: {
         aspectRatio: Utopia.stringControl(),
         crop: Utopia.stringControl(),
-        data: Utopia.objectControl({
-          altText: Utopia.stringControl(),
-          id: Utopia.stringControl(),
-          url: Utopia.stringControl(),
-          height: Utopia.numberControl(),
-          width: Utopia.numberControl(),
-        }),
+        data: ImageTypeControl,
         // loader is omitted, we don't have a control description for functions yet
         srcSetOptions: Utopia.objectControl({
-            intervals: Utopia.numberControl(),
-            startingWidth: Utopia.numberControl(),
-            incrementSize: Utopia.numberControl(),
-            placeholderWidth: Utopia.numberControl(),
-        })
+          intervals: Utopia.numberControl(),
+          startingWidth: Utopia.numberControl(),
+          incrementSize: Utopia.numberControl(),
+          placeholderWidth: Utopia.numberControl(),
+        }),
       },
       supportsChildren: false,
       variants: [],
