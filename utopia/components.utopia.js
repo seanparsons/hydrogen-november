c6fe346ae9be7004e21b85508f2537e1c4e84737
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-04-02 18:15:04 +0200

    Money props

--- utopia/components.utopia.js
+++ utopia/components.utopia.js
@@ -1,5 +1,5 @@
 import * as Utopia from 'utopia-api'
-import { Image } from '@shopify/hydrogen'
+import { Image, Money } from '@shopify/hydrogen'
 import { RecommendedProductsGrid } from '../app/components/Layout'
 
 const ImageCropControl = Utopia.expressionPopupListControl([
@@ -18,6 +18,69 @@ const ImageTypeControl = Utopia.objectControl({
   width: Utopia.numberControl(),
 })
 
+const CurrencyCodeControl = Utopia.popupListControl([
+  {
+    value: 'AED',
+    label: 'United Arab Emirates Dirham (AED)',
+  },
+  {
+    value: 'AFN',
+    label: 'Afghan Afghani (AFN).',
+  },
+  // TODO: all currencies
+])
+
+const UnitPriceMeasurementMeasuredUnit =
+  Utopia.popupListControl([
+    {
+      label: 'CL',
+      value: 'va100 centiliters equals 1 liter.',
+    },
+    {
+      value: 'CM',
+      label: '100 centimeters equals 1 meter.',
+    },
+    {
+      value: 'G',
+      label: 'Metric system unit of weight.',
+    },
+    {
+      value: 'KG',
+      label: '1 kilogram equals 1000 grams.',
+    },
+    // TODO: all values
+  ])
+
+const MoneyV2Control = Utopia.objectControl({
+  amount: Utopia.numberControl(),
+  currency: CurrencyCodeControl,
+})
+
+const UnitPriceMeasurementControl = Utopia.objectControl({
+  measuredType: Utopia.popupListControl([
+    {
+      value: 'AREA',
+      label: 'Unit of measurements representing areas.',
+    },
+    {
+      value: 'LENGTH',
+      label: 'Unit of measurements representing lengths.',
+    },
+    {
+      value: 'VOLUME',
+      label: 'Unit of measurements representing volumes.',
+    },
+    {
+      value: 'WEIGHT',
+      label: 'Unit of measurements representing weights.',
+    },
+  ]),
+  quantityUnit: UnitPriceMeasurementMeasuredUnit,
+  quantityValue: Utopia.numberControl(),
+  referenceUnit: UnitPriceMeasurementMeasuredUnit,
+  referenceValue: Utopia.numberControl(), // this can only be int
+})
+
 const Components = {
   '@shopify/hydrogen': {
     // https://shopify.dev/docs/api/hydrogen-react/2024-01/components/image
@@ -34,6 +97,18 @@ const Components = {
           incrementSize: Utopia.numberControl(),
           placeholderWidth: Utopia.numberControl(),
         }),
+      },
+      supportsChildren: false,
+      variants: [],
+    },
+    Money: {
+      component: Money,
+      properties: {
+        data: MoneyV2Control,
+        withoutCurrency: Utopia.checkboxControl(),
+        withoutTrailingZeros: Utopia.checkboxControl(),
+        measurement: UnitPriceMeasurementControl,
+        measurementSeparator: Utopia.jsxControl(),
       },
       supportsChildren: false,
       variants: [],
