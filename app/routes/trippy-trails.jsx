ff1ffe08116e3b46fc84236553f72835a7c7ef65
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2024-04-22 13:18:16 +0200

    placeholder image for the desired design

--- app/routes/trippy-trails.jsx
+++ app/routes/trippy-trails.jsx
@@ -3,8 +3,20 @@ import { BlogComponent } from '../components/Layout'
 
 export default function Blog() {
   return (
-    <BlogComponent title={<h1>Hello!</h1>}>
-      hi!
-    </BlogComponent>
+    <>
+      <div
+        style={{
+          position: 'absolute',
+          top: 0,
+          width: 1440,
+          height: 9599,
+          backgroundImage:
+            'url(https://cdn.discordapp.com/attachments/703248722334842910/1231926200054583316/trippytrails.jpg?ex=6638bbae&is=662646ae&hm=0e5a7fd4ed51a811416af3dd6a93f8d0baf5b3956a3023bb4714579c81d597c0&)',
+        }}
+      ></div>
+      <BlogComponent title={<h1>Hello!</h1>}>
+        hi!
+      </BlogComponent>
+    </>
   )
 }
